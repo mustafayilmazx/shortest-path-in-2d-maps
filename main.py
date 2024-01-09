ed5b920ae6d6fd63c6b868b4e5fe6011bd0dc149
Mustafa Yılmaz <yilmaz.mustafax@gmail.com>
2024-01-10 02:20:12 +0300

    add timing measurements

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from dijkstra import find_path_with_dijkstra
 from utils import load_image
 from a_star import find_path_with_a_star
 from lee import find_path_with_lee
-
+import time
 image = load_image("ev.png")
+
+start = time.time()
 find_path_with_dijkstra(image)
+end = time.time()
+print("Dijkstra: ", end - start)
+
+start = time.time()
 find_path_with_a_star(image)
+end = time.time()
+print("A*: ", end - start)
+
+start = time.time()
 find_path_with_lee(image)
+end = time.time()
+print("Lee: ", end - start)
